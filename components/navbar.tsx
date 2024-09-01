e6e14b50f299ae1e3f8984c77ee3e31d3411338a
Salil Mandal <91447641+Asta1984@users.noreply.github.com>
2024-09-01 17:53:02 +0530

    Update navbar.tsx
    tt

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -74,7 +74,7 @@ export default function Navbar() {
                 href="/faq"
                 className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
               >
-                FAQ's
+                FAQ&apos;s  {/* Escaped single quote */}
               </Link>
             </div>
           </div>
@@ -130,7 +130,7 @@ export default function Navbar() {
               href="/faq"
               className="block pl-3 pr-4 py-2 border-l-4 text-base font-medium border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"
             >
-              FAQ's
+              FAQ&apos;s  {/* Escaped single quote */}
             </Link>
           </div>
           <div className="pt-4 pb-3 border-t border-gray-200">
